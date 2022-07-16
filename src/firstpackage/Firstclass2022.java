0d34b88959089d70c19a4717ea6d0b3c0d70a86f
gauravp2211 <gauravpatne11@gmail.com>
2022-07-17 00:51:43 +0530

    hello gaurav

---
+++ src/firstpackage/Firstclass2022.java
@@ -0,0 +1,14 @@
+package firstpackage;
+
+public class Firstclass2022 {
+	public static void main(String[] args) {
+		
+		int i=0;	
+		System.out.print(i);
+		while(i<=10)
+		{
+			System.out.print(i);
+			i+=2;
+		}
+}
+}
